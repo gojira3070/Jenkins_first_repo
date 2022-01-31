2fa0039bd161ffe40d108f5c0c2fa2f041b31448
Gogira3070 <mjacob971010@gmail.com>
2022-01-31 11:13:25 +0100

    suppression et création de main

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ mkdir -p /tmp/ajc
 rm -rf /tmp/ajc/*
 echo '
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, World !") ;
+        public static void main(String[] args) {
+                System.out.println("Hello, World !") ;
     }
 }
 ' > /tmp/ajc/Main.java
